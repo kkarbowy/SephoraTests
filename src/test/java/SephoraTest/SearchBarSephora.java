42171db1a20ecf048580438c493869ad5f1f3247
kkarbowy <k.karbowy2401@gmail.com>
2024-05-24 23:37:56 +0200

    Added getProductsList method and use it in my test

--- src/test/java/SephoraTest/SearchBarSephora.java
+++ src/test/java/SephoraTest/SearchBarSephora.java
@@ -1,13 +1,19 @@
 package SephoraTest;
 
+import com.beust.ah.A;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class SearchBarSephora {
 
@@ -24,8 +30,17 @@ public class SearchBarSephora {
     }
     @Test
     public void checkIfFoundPharasesAreProperly() { //to będę sprawdzać
+
+        String expectedPhrase = "rituals";
+
         //włączenie przeglądarki, znalezienie wyszukiwarki, wpisanie frazy i enter
-        findWrittenPhrase("rituals");
+        findWrittenPhrase(expectedPhrase);
+        ArrayList<String> products = getProductsList();
+        System.out.println(products);
+        for (String product : products) {
+            product = product.toLowerCase();
+            Assert.assertEquals(product, expectedPhrase, "Nazwy nie są zgodne!");
+        }
 
     }
 
@@ -39,6 +54,14 @@ public class SearchBarSephora {
         //zatwierdź
         searchBar.sendKeys(Keys.RETURN);
     }
-
+    private ArrayList<String> getProductsList() {
+        ArrayList<String> products = new ArrayList<>();
+        List<WebElement> nameLocalisation = driver.findElements(By.xpath("//*[@id=\"search-result-items\"]/li/div/div[3]/div/a/span"));
+        for (WebElement element : nameLocalisation) {
+            String elementText = element.getText();
+            products.add(elementText);
+        }
+        return products;
+    }
 
 }
